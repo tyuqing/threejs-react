3c148ed4eac56abd92033582448dcc5dac35263e
tyuqing <tyuqing7@163.com>
2023-03-14 14:51:50 +0800

    refactor: eslint

--- src/pages/jifang/index.jsx
+++ src/pages/jifang/index.jsx
@@ -1,79 +1,88 @@
-
 import { useEffect, useRef } from 'react';
 import {
-	Scene, Color,DirectionalLight, PerspectiveCamera, WebGLRenderer, AxesHelper,
-	CameraHelper, AmbientLight
+  AmbientLight,
+  AxesHelper,
+  CameraHelper,
+  Color,
+  DirectionalLight,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
 } from 'three';
-import { loaderObj } from '@/utils/loader';
 import { loaderGltf } from '../../utils/loader';
 
 function JiFang() {
+  const container = useRef(null);
 
-	const container = useRef(null)
+  //全局变量
+  const w = 1200;
+  const h = 720;
+  const r = w / h;
+  const s = 330;
 
-	//全局变量
-	const w = 1200;
-	const h = 720;
-	const r = w / h;
-	const s = 330;
+  //场景
+  const scene = new Scene();
+  scene.background = new Color(0x666666);
 
-	//场景
-	const scene = new Scene();
-	scene.background = new Color(0x666666);
+  //光照
+  const aLight = new AmbientLight(0x404040, 1);
+  scene.add(aLight);
+  const dLight = new DirectionalLight(0xffffff, 1);
+  scene.add(dLight);
 
-	//光照
-	const aLight = new AmbientLight(0x404040, 1);
-	scene.add(aLight);
-	const dLight = new DirectionalLight(0xffffff, 1);
-	scene.add(dLight);
+  //照相机
+  const camera = new PerspectiveCamera(75, r, 1, 1000);
+  //const camera = new THREE.OrthographicCamera(-s*r, s*r, s, -s, 1, 1000);
+  camera.position.set(300, 200, 300);
+  camera.lookAt(scene.position);
 
-	//照相机
-	const camera = new PerspectiveCamera(75, r, 1, 1000);
-	//const camera = new THREE.OrthographicCamera(-s*r, s*r, s, -s, 1, 1000);
-	camera.position.set(300, 200, 300);
-	camera.lookAt(scene.position);
+  //辅助对象 坐标
+  const axis = new AxesHelper(300);
+  scene.add(axis);
+  const cHelper = new CameraHelper(camera);
+  scene.add(cHelper);
 
-	//辅助对象 坐标
-	const axis = new AxesHelper(300);
-	scene.add(axis);
-	const cHelper = new CameraHelper(camera);
-	scene.add(cHelper);
+  //渲染器
+  const renderer = new WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(w, h);
+  renderer.setClearColor(0xffffff, 1);
+  useEffect(() => {
+    container?.current?.appendChild(renderer.domElement);
+  }, [container]);
 
-	//渲染器
-	const renderer = new WebGLRenderer({
-		antialias: true
-	});
-	renderer.setSize(w, h);
-	renderer.setClearColor(0xffffff, 1);
-	useEffect(() => {
-		container?.current?.appendChild(renderer.domElement);
-	}, [container])
+  //控制器
+  // const ctrl = new OrbitControls(camera, renderer.domElement);
 
-	//控制器
-	// const ctrl = new OrbitControls(camera, renderer.domElement);
+  //渲染函数
+  function render() {
+    //console.log("Render...");
+    renderer.render(scene, camera);
 
-	//渲染函数
-	function render() {
-		//console.log("Render...");
-		renderer.render(scene, camera);
+    //  一直旋转
+    scene.rotation.y += 0.002;
 
-		//  一直旋转
-		scene.rotation.y += 0.002;
+    requestAnimationFrame(render);
+  }
 
-		requestAnimationFrame(render);
-	}
+  //加载模型
+  // loaderObj({ objSrc: "/images/obj/server/uploads_files_2840457_server.obj", mtlSrc: '/images/obj/server/server.mtl' }, function (obj) {
+  // 	scene.add(obj);
+  // 	render();
+  // })
+  loaderGltf(
+    {
+      gltfSrc: '/images/gltf/server/server.gltf',
+      mtlSrc: '/images/obj/server/server.mtl',
+    },
+    function (model) {
+      scene.add(model);
+      render();
+    }
+  );
 
-	//加载模型
-	// loaderObj({ objSrc: "/images/obj/server/uploads_files_2840457_server.obj", mtlSrc: '/images/obj/server/server.mtl' }, function (obj) {
-	// 	scene.add(obj);
-	// 	render();
-	// })
-	loaderGltf({ gltfSrc: "/images/gltf/server/server.gltf", mtlSrc: '/images/obj/server/server.mtl' }, function (model) {
-			scene.add(model);
-			render();
-		})
-
-	return (<div ref={container}></div>);
+  return <div ref={container}></div>;
 }
 
 export default JiFang;
--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,86 +1,77 @@
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { AnimationMixer } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import {AnimationMixer} from 'three'
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 /**
  * 载入obj模型
  */
-export function loaderObj({objSrc, mtlSrc} = {}, callback) {
+export function loaderObj({ objSrc, mtlSrc } = {}, callback) {
+  // 创建loader
+  const objLoad = new OBJLoader();
+  const mtlLoader = new MTLLoader();
 
-	// 创建loader
-	const objLoad = new OBJLoader();
-	const mtlLoader = new MTLLoader();
-
-  
-	mtlLoader.load(
-		// mtl文件的路径 
-		mtlSrc,
-		// 回调函数，参数是材质对象 
-		function (materials) {
-			materials.preload()
-			objLoad
-				.setMaterials(materials)
-				.load(
-					objSrc,
-					function (obj) {
-						console.log("Model: ", obj);
-						obj.scale.set(150, 150, 150);
-						obj.position.y = 0;
-            callback && callback(obj)
-					},
-					function (xhr) {
-						console.log("Loaded: ", xhr);
-					},
-					function (err) {
-						console.error("Error: ", err);
-					}
-				);
-		})
+  mtlLoader.load(
+    // mtl文件的路径
+    mtlSrc,
+    // 回调函数，参数是材质对象
+    function (materials) {
+      materials.preload();
+      objLoad.setMaterials(materials).load(
+        objSrc,
+        function (obj) {
+          console.log('Model: ', obj);
+          obj.scale.set(150, 150, 150);
+          obj.position.y = 0;
+          callback && callback(obj);
+        },
+        function (xhr) {
+          console.log('Loaded: ', xhr);
+        },
+        function (err) {
+          console.error('Error: ', err);
+        }
+      );
+    }
+  );
 }
 
-export function loaderGltf({gltfSrc, mtlSrc} = {}, callback) {
+export function loaderGltf({ gltfSrc, mtlSrc } = {}, callback) {
+  // 创建loader
+  const gltfLoad = new GLTFLoader();
+  const mtlLoader = new MTLLoader();
 
-	// 创建loader
-	const gltfLoad = new GLTFLoader();
-	const mtlLoader = new MTLLoader();
-
-// 加载gltf文件
-gltfLoad.load(
-  // 文件路径
-  gltfSrc,
-  // 回调函数
-  function ( gltf ) {
-		console.log(gltf)
+  // 加载gltf文件
+  gltfLoad.load(
+    // 文件路径
+    gltfSrc,
+    // 回调函数
+    function (gltf) {
+      console.log(gltf);
       // 获取返回的gltf对象
       var model = gltf.scene;
       var animations = gltf.animations;
       model.scale.set(100, 100, 100);
-			model.position.y = 0
+      model.position.y = 0;
 
       // 将模型添加到场景中
-      callback&& callback( model );
+      callback && callback(model);
 
       // 如果有动画，创建一个AnimationMixer对象，并将动画添加到它里面
-      if ( animations && animations.length ) {
-          mixer = new AnimationMixer( model );
-          for ( var i = 0; i < animations.length; i ++ ) {
-              mixer.clipAction( animations[ i ] ).play();
-          }
+      if (animations && animations.length) {
+        mixer = new AnimationMixer(model);
+        for (var i = 0; i < animations.length; i++) {
+          mixer.clipAction(animations[i]).play();
+        }
       }
-
-  },
-  // 可选的进度回调函数
-  function ( xhr ) {
-
-      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-  },
-  // 可选的错误回调函数
-  function ( error ) {
-
-      console.log( 'An error happened' );
-
-  }
-);
+    },
+    // 可选的进度回调函数
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+    // 可选的错误回调函数
+    function (error) {
+      console.log('An error happened');
+    }
+  );
 }
